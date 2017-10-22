80a4414d22ed46458f8f16873aee809d60f37c43
Cyrus Cowley <cyficowley@gmail.com>
2017-10-21 23:04:31 -0700

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,17 +1,18 @@
 from flask_restful import Resource, Api
 from flask import Flask
 from wikisort import wordRelevance
+from wikisort import summaries
 
 app = Flask(__name__)
 api = Api(app)
 
 class WikiTitle(Resource):
     def get(self,topic):
-        return {'titles': wordRelevance(topic).titles}
+        return {'titles': wordRelevance(topic)}
 
 class WikiSummary(Resource):
     def get(self,topic):
-        return {'summaries': summaries(topics).summaries}
+        return {'summaries': summaries(topics)}
 
     
 
