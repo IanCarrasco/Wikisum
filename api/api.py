3c32b3cba6a8d4acfe3a85d17b89936a6682e00f
Ian Carrasco <iansee.c@gmail.com>
2017-10-21 20:30:00 -0700

    API Update for Summary Calls

--- api/api.py
+++ api/api.py
@@ -5,21 +5,19 @@ from wikisort import wordRelevance
 app = Flask(__name__)
 api = Api(app)
 
-class WikiInfo(Resource):
+class WikiTitle(Resource):
     def get(self,topic):
-        return {'summary': wordRelevance(topic)}
+        return {'titles': wordRelevance(topic).titles}
+
+class WikiSummary(Resource):
+    def get(self,topic):
+        return {'summaries': summaries(topics).summaries}
 
     
 
-
-api.add_resource(WikiInfo, '/wiki/<string:topic>')
+api.add_resource(WikiTitle, '/wiki/<string:topic>/titles')
+api.add_resource(WikiSummary, '/wiki/<string:titles>/summaries')
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-# wordRelevance('iPhone')
-
-wordRelevance('Python Language')
-
-#use summary links
